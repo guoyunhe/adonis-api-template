4a990aa7b003dda78a49212246430754d60326b2
Guo Yunhe <i@guoyunhe.me>
2023-05-22 21:33:14 +0800

    change timestamp sql

--- database/migrations/1668258617995_users.ts
+++ database/migrations/1668258617995_users.ts
@@ -13,8 +13,8 @@ export default class extends BaseSchema {
       table.string('remember_me_token').nullable();
       table.string('role').nullable();
 
-      table.timestamp('created_at', { useTz: true }).notNullable();
-      table.timestamp('updated_at', { useTz: true }).notNullable();
+      table.timestamp('created_at').nullable();
+      table.timestamp('updated_at').nullable();
     });
   }
 
