96187418837eb90ace08170760d7c90e0e5aa6f4
Guo Yunhe <i@guoyunhe.me>
2023-05-25 22:21:43 +0800

    avatar upload

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,6 +1,10 @@
+import { Attachment } from '@ioc:Adonis/Addons/AttachmentLite';
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 import { rules, schema } from '@ioc:Adonis/Core/Validator';
 import User from 'App/Models/User';
+import { rename } from 'fs/promises';
+import sharp from 'sharp';
+import { file } from 'tmp-promise';
 
 export default class AuthController {
   public async login({ auth, request, response }: HttpContextContract) {
@@ -24,26 +28,83 @@ export default class AuthController {
   }
 
   public async register({ auth, request }: HttpContextContract) {
-    const validations = await schema.create({
-      name: schema.string({ trim: true }, [rules.maxLength(255)]),
-      email: schema.string({ trim: true }, [
-        rules.email(),
-        rules.unique({ table: 'users', column: 'email', caseInsensitive: true }),
-        rules.maxLength(255),
-      ]),
-      password: schema.string({ trim: true }, [
-        rules.minLength(8),
-        rules.maxLength(32),
-        rules.confirmed('passwordConfirm'),
-      ]),
+    const data = await request.validate({
+      schema: schema.create({
+        name: schema.string({ trim: true }, [rules.maxLength(255)]),
+        username: schema.string({ trim: true }, [
+          rules.alphaNum({ allow: ['underscore'] }),
+          rules.unique({ table: 'users', column: 'username', caseInsensitive: true }),
+          rules.maxLength(255),
+        ]),
+        email: schema.string({ trim: true }, [
+          rules.email(),
+          rules.unique({ table: 'users', column: 'email', caseInsensitive: true }),
+          rules.maxLength(255),
+        ]),
+        password: schema.string({ trim: true }, [
+          rules.minLength(8),
+          rules.maxLength(32),
+          rules.confirmed('passwordConfirm'),
+        ]),
+      }),
     });
-    const data = await request.validate({ schema: validations });
     const user = await User.create(data);
     const token = await auth.use('api').attempt(data.email, data.password);
     return { user, token };
   }
 
-  public async user({ auth }: HttpContextContract) {
+  public async show({ auth }: HttpContextContract) {
     return auth.user;
+  }
+
+  public async update({ auth, request }: HttpContextContract) {
+    const { avatar, name, username, email, password } = await request.validate({
+      schema: schema.create({
+        avatar: schema.file.optional({
+          size: '20mb',
+          extnames: ['jpg', 'gif', 'png', 'webp'],
+        }),
+        name: schema.string.optional({ trim: true }, [rules.maxLength(255)]),
+        username: schema.string.optional({ trim: true }, [
+          rules.alphaNum({ allow: ['underscore'] }),
+          rules.unique({
+            table: 'users',
+            column: 'username',
+            caseInsensitive: true,
+            whereNot: { id: auth.user!.id },
+          }),
+          rules.maxLength(255),
+        ]),
+        email: schema.string.optional({ trim: true }, [
+          rules.email(),
+          rules.unique({
+            table: 'users',
+            column: 'email',
+            caseInsensitive: true,
+            whereNot: { id: auth.user!.id },
+          }),
+          rules.maxLength(255),
+        ]),
+        password: schema.string.optional({ trim: true }, [
+          rules.minLength(8),
+          rules.maxLength(32),
+          rules.confirmed('passwordConfirm'),
+        ]),
+      }),
+    });
+
+    if (avatar?.tmpPath) {
+      const tempFile = await file();
+      await sharp(avatar.tmpPath)
+        .resize({ width: 512, height: 512, fit: 'cover' })
+        .webp()
+        .toFile(tempFile.path);
+      await rename(tempFile.path, avatar.tmpPath);
+      auth.user!.avatar = Attachment.fromFile(avatar);
+    }
+
+    auth.user!.save();
+
+    return await User.find(auth.user!.id);
   }
 }
--- app/Models/User.ts
+++ app/Models/User.ts
@@ -4,7 +4,7 @@ import { beforeSave, column } from '@ioc:Adonis/Lucid/Orm';
 import Model from './Model';
 
 export default class User extends Model {
-  @attachment({ folder: 'avatars' })
+  @attachment({ folder: 'avatars', preComputeUrl: true })
   public avatar: AttachmentContract | null;
 
   @column()
